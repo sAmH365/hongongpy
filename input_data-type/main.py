58622e0f97101919ec0c09c8fac0a56dc693bd50
sAmH365 <npower5377@naver.com>
2024-12-02 08:46:10 +0900

    13강까지

---
+++ input_data-type/main.py
@@ -0,0 +1,13 @@
+print("=======표준입력========")
+a = input("입력해주세요: ")
+b = input(">>> ")
+
+print(type(a))
+print(a + b)
+
+a = input("숫자1: ")
+b = input("숫자2: ")
+
+print(a + b)
+print(int(a) + float(b))
+print(int(a) + int(b))
